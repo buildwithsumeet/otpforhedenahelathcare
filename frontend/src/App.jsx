6702951e0d3772db67929bf125de0558864f360d
Sumeet Singh <mr.sumeet8114@gmail.com>
2025-08-17 18:19:52 +0530

    fix all the bug by sumeet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,11 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/page/home/home.jsx';
-// import About from './pages/About';
-// import Products from './pages/Products';
-=======
 import React from 'react'
 import Sidebar from './components/sidebar/Sidebar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard'
->>>>>>> ankita
 
 function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} /> */}
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
-=======
       <div className="flex">
         {/* Sidebar fixed on the left */}
         <div className="w-64 min-h-screen bg-white shadow-md">
@@ -43,4 +24,3 @@ export default App;
 }
 
 export default App
->>>>>>> ankita
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-=======
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
->>>>>>> ankita
 })
