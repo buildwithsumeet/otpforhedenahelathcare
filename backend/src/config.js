2b6359c407cce599bfff2e4cd63525e491b2e193
taskjionline <taskjionline@gmail.com>
2026-03-20 21:24:20 +0530

    fix

--- backend/src/config.js
+++ backend/src/config.js
@@ -6,4 +6,4 @@ export const BITRIX_TOKEN = "hs30qqnntt6ch7sopulypscu247z6qgj";
 export const RAZORPAY_KEY_ID = "rzp_test_SRrZ8YKuF5tkyr"
 export const RAZORPAY_KEY_SECRET = "KSL6J7XjIWc1A2QiUFz0n9ss"
 
-export const FRONTEND_URL = "https://yourfrontend.com"
+export const FRONTEND_URL = "http://187.127.131.10::80"
