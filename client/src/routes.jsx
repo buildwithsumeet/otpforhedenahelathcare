c133870fec7066a8d436167a90f7fb4822557b70
taskjionline <taskjionline@gmail.com>
2026-03-20 14:10:47 +0530

    update innix.confi

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,8 +2,14 @@ import { createBrowserRouter } from "react-router-dom"
 import PaymentPage from "./pages/PaymentPage"
 import SuccessPage from "./pages/SuccessPage"
 import FailedPage from "./pages/FailedPage"
+import App from "./App"
 
 export const router = createBrowserRouter([
+
+    {
+        path:"/",
+        element: <App />
+    },
   {
     path: "/pay/:booking_id",
     element: <PaymentPage />
