ce25c22b0a89f205a33e6a74b5189eb311b44e9f
taskjionline <taskjionline@gmail.com>
2026-03-22 09:39:45 +0530

    fix

--- client/src/api/paymentApi.js
+++ client/src/api/paymentApi.js
@@ -4,8 +4,9 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL
 })
 
-export const createOrder = (deal_id, amount) =>
-  API.post("/payment/create-order", { deal_id, amount })
+
+export const createOrder = (data) =>
+  API.post("/payment/create-order", data)
 
 export const verifyPayment = (data) =>
   API.post("/payment/verify", data)
