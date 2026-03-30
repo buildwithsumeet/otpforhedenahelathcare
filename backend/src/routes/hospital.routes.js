990577d70bdeab5132b42f8efa2b8e2e47785ec4
taskjionline <taskjionline@gmail.com>
2026-03-30 11:57:43 +0530

    fix

--- backend/src/routes/hospital.routes.js
+++ backend/src/routes/hospital.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { registerHospitalFromBitrix, saveHospital } from "../controllers/hospital.controller.js";
+import {  saveHospital } from "../controllers/hospital.controller.js";
 
 const router = Router();
 
 // Bitrix Webhook: Hospital Create/Update
-router.post("/register", registerHospitalFromBitrix);
+// router.post("/register", registerHospitalFromBitrix);
 
 // Save hospital directly (without Bitrix webhook)
 router.post("/save", saveHospital);
